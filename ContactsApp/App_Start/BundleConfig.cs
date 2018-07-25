226fb7069d2cf16ae382f3ba69c706a487848fe2
Pedro Capriles <pedro.capriles@arkanosoft.com>
2018-07-24 21:25:57 -0400

    Add project files.

---
+++ ContactsApp/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ContactsApp
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+
+            bundles.Add(new StyleBundle("~/Content/CSS/Bootstrap").Include(
+                      "~/Content/CSS/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/CSS/Styles").Include(
+                       "~/Content/CSS/Styles.css"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/Jquery").Include(
+                "~/Scripts/Jquery/jquery-3.3.1.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/Bootstrap").Include(
+                        "~/Scripts/Bootstrap/bootstrap.js"));
+
+        }
+    }
+}
---
+++ ContactsApp/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactsApp.Models
+{
+    public class Contact
+    {
+        #region Attributes
+            public int ContactID { get; set; }
+
+            public string Name { get; set; }
+
+            public string LastName { get; set; }
+
+            public Byte ProfilePic { get; set; }
+
+            public string  Address { get; set; }
+
+            public string Phone { get; set; }
+
+            public string Email { get; set; }
+        #endregion
+    }
+}
